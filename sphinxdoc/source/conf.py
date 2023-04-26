2bcfc016bc451104fba15a419008b3ea4a3fb77f
João Pedro <jpcaveiro@users.noreply.github.com>
2023-04-26 14:13:19 +0100

    deleted the publication of source files

--- sphinxdoc/source/conf.py
+++ sphinxdoc/source/conf.py
@@ -61,3 +61,8 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+#Avoid generating sources (*.rst files)
+html_copy_source = False
+
